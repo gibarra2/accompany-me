700a6f338433766a759cd07aa6ed08cc88a9028b
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-08-04 15:49:22 -0500

    Removed check constraint from Place model

--- trips/models/place.py
+++ trips/models/place.py
@@ -21,12 +21,6 @@ class Place(models.Model):
     category = models.CharField(max_length = 4, choices = CATEGORIES)
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name = 'places')
 
-    # #Want to add constraint that place date must be between trip start_date and trip end_date
-    # class Meta:
-    #     constraints = [
-    #         models.CheckConstraint(check=Q(date__gte= trip__start_date), name="date is after trip start date")
-    #     ]
-    # Also, want to add constraint that place can only be added to a trip once   
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['name', 'trip'], name='Place can only be added to trip once'),
